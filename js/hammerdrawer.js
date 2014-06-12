a2203497935dc8809474490d41a7cb75628d06bc
Jared Anderson <jared.anderson@gatewaycc.edu>
2014-06-11 17:25:49 -0700

    Daily build...

---
+++ js/hammerdrawer.js
@@ -0,0 +1,119 @@
+(function ($) {
+
+"use strict";
+
+// element (the drawer)
+// config: siblings, handler, parent, options
+// config.options: the type of drawer / base class(es) to init & toggle
+var HammerDrawer = function(drawer, config ){
+
+    this.isactivated            =   config.activated;
+    this.classes                =   {};
+    this.classes.base           =   config.options.replace(/(\S+)/g, "hammerdrawer-$1");
+
+    this.classes.activated      =   this.classes.base.replace(/(\S+)/g, "$1-open hammerdrawer-open");
+    this.classes.deactivated    =   this.classes.base.replace(/(\S+)/g, "$1-close hammerdrawer-close");
+    this.reset_timeout          =   config.reset_timeout;
+
+
+    this.$drawer                =   $(drawer);
+    this.$siblings              =   $(config.siblings);
+    this.$toggler               =   $(config.toggle);
+    this.$opener                =   $(config.open);
+    this.$closer                =   $(config.close);
+    this.$parent                =   $(config.parent);
+    this.$items                 =   this.$drawer.add(this.$siblings).add(this.$toggler).add(this.$parent);
+
+    this.initClasses().initEvents();
+    if(this.activated)
+        this.open();
+
+}
+
+
+HammerDrawer.prototype.initClasses=function(){
+
+    this.$drawer.addClass(this.classes.base + "-drawer");
+
+    this.$siblings.addClass(this.classes.base + '-sibling');
+
+    this.$toggler.addClass(this.classes.base + '-toggler');
+
+    this.$parent.addClass(this.classes.base + '-parent');
+
+    return this;
+}
+
+
+HammerDrawer.prototype.initEvents=function(){
+    var hammerdrawer = this;
+
+    this.$toggler.on("click", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        hammerdrawer.toggle();
+    });
+
+    this.$siblings.click(function() {
+        if (hammerdrawer.isactivated) {
+            hammerdrawer.close();
+        }
+    });
+
+    return this;
+
+}
+
+HammerDrawer.prototype.reset=function(){
+    this.isactivated=false;
+    this.$items.removeClass(this.classes.activated).removeClass(this.classes.deactivated);
+}
+
+
+HammerDrawer.prototype.open=function(){
+    this.isactivated=true;
+    this.$items.removeClass( this.classes.deactivated ).addClass( this.classes.activated );
+}
+
+HammerDrawer.prototype.close=function(){
+    this.$items.removeClass(this.classes.activated).addClass(this.classes.deactivated);
+    setTimeout($.proxy(this.reset, this), this.reset_timeout);
+}
+
+HammerDrawer.prototype.toggle=function(){
+    this.isactivated ? this.close() : this.open();
+}
+
+HammerDrawer.defaults = {
+    activate:false,
+    options:"",
+    open:null,
+    close:null,
+    toggle:null,
+    reset_timeout:1*1000*.5,
+    onOpenStart:function(hammerdrawer){},
+    onOpenEnd:function(hammerdrawer){},
+    onCloseStart:function(hammerdrawer){},
+    onCloseEnd:function(hammerdrawer){}
+}
+
+$.fn.hammerdrawer=function(options){
+    return $(this).each(function(){
+        var config = $.extend(
+            HammerDrawer.defaults,
+            {
+                'siblings':$(this).siblings(),
+                'parent':$(this).parent()
+            },
+            options
+        );
+        var $element = $(this);
+        if (! $element.data('hammerdrawer-obj') ) {
+            $element.data('hammerdrawer-obj', new HammerDrawer( $element, config ) );
+        }
+    })
+}
+
+
+
+}(jQuery));
--- js/hammernav.js
+++ js/hammernav.js
@@ -2,18 +2,25 @@
 
 "use strict";
 
-var HammerNavDrawer = function(nav, siblings, toggler, parent, options ){
+// element (the drawer)
+// config: siblings, handler, parent, options
+// config.options: the type of drawer / base class(es) to init & toggle
+var HammerNavDrawer = function(element, config ){
 
     this.isactivated            =   false;
     this.classes                =   {};
-    this.classes.default        =   options.replace(/(\S+)/g, "hammernav-$1");
-    this.classes.activated      =   this.classes.default.replace(/(\S+)/g, "$1-open");
-    this.classes.deactivated    =   this.classes.default.replace(/(\S+)/g, "$1-close");
+    this.classes.base           =   config.options.replace(/(\S+)/g, "hammernav-$1");
 
-    this.$nav                   =   $(nav);
-    this.$siblings              =   $(siblings);
-    this.$toggler               =   $(toggler);
-    this.$parent                =   $(parent);
+    // this.classes.default        =   options.replace(/(\S+)/g, "hammernav-$1");
+    this.classes.activated      =   this.classes.base.replace(/(\S+)/g, "$1-open hammernav-open");
+    this.classes.deactivated    =   this.classes.base.replace(/(\S+)/g, "$1-close hammernav-close");
+
+    this.$nav                   =   $(element);
+    this.$siblings              =   $(config.siblings);
+    this.$toggler               =   $(config.toggle);
+    this.$opener                =   $(config.open);
+    this.$closer                =   $(config.close);
+    this.$parent                =   $(config.parent);
     this.$items                 =   this.$nav.add(this.$siblings).add(this.$toggler).add(this.$parent);
 
     this.initClasses().initEvents()
@@ -22,10 +29,15 @@ var HammerNavDrawer = function(nav, siblings, toggler, parent, options ){
 
 
 HammerNavDrawer.prototype.initClasses=function(){
-    this.$nav.addClass('hammernav-nav ' + this.classes.default);
-    this.$siblings.addClass('hammernav-sibling ' + this.classes.default);
-    this.$toggler.addClass('hammernav-toggler ' + this.classes.default);
-    this.$parent.addClass('hammernav-parent ' + this.classes.default);
+
+    this.$nav.addClass(this.classes.base + "-nav");
+
+    this.$siblings.addClass(this.classes.base + '-sibling');
+
+    this.$toggler.addClass(this.classes.base + '-toggler');
+
+    this.$parent.addClass(this.classes.base + '-parent');
+
     return this;
 }
 
@@ -68,6 +80,22 @@ HammerNavDrawer.prototype.close=function(){
 HammerNavDrawer.prototype.toggle=function(){
     this.isactivated ? this.close() : this.open();
 }
+
+
+HammerNavDrawer.defaults = {
+
+    options:"",
+    open:null,
+    close:null,
+    toggle:null,
+    onOpenStart:function(hammernav){},
+    onOpenEnd:function(hammernav){},
+    onCloseStart:function(hammernav){},
+    onCloseEnd:function(hammernav){}
+
+}
+
+
 
 
 var HammerNav = function(nav, siblings, toggler, parent, options ){
@@ -171,19 +199,6 @@ HammerNav.prototype.toggle=function(){
 }
 
 
-HammerNav.defaults = {
-
-    submenus:" ul li > a ~ ul",
-    submenu_sticky_selector:'active',
-    sumenu_expander:true,
-    options:"left",
-    onOpenStart:function(hammernav){},
-    onOpenEnd:function(hammernav){},
-    onCloseStart:function(hammernav){},
-    onCloseEnd:function(hammernav){}
-
-}
-
 
 
 $.fn.hammernavdrawer=function(options){
@@ -192,27 +207,26 @@ $.fn.hammernavdrawer=function(options){
             HammerNavDrawer.defaults,
             {
                 'siblings':$(this).siblings(),
-                'toggler':null,
                 'parent':$(this).parent()
             },
             options
         );
         var $element = $(this);
         if (! $element.data('hammernavdrawer-obj') ) {
-            $element.data('hammernavdrawer-obj', new HammerNavDrawer( $element, config.siblings, config.toggler, config.parent, config.options ) );
+            $element.data('hammernavdrawer-obj', new HammerNavDrawer( $element, config ) );
         }
     })
 }
 
-$.fn.hammernav=function(options){
-    return $(this).each(function(){
-        var config = $.extend( HammerNav.defaults, { 'siblings':$(this).siblings(), 'toggler':'.hammernav-toggler', 'parent':$(this).parent() }, options);
-        var $element = $(this);
-        if (! $element.data('hammernav-obj') ) {
-            $element.data('hammernav-obj', new HammerNav( $element, config.siblings, config.toggler, config.parent, config.options ) );
-        }
-    })
-}
+// $.fn.hammernav=function(options){
+//     return $(this).each(function(){
+//         var config = $.extend( HammerNav.defaults, { 'siblings':$(this).siblings(), 'toggler':'.hammernav-toggler', 'parent':$(this).parent() }, options);
+//         var $element = $(this);
+//         if (! $element.data('hammernav-obj') ) {
+//             $element.data('hammernav-obj', new HammerNav( $element, config.siblings, config.toggler, config.parent, config.options ) );
+//         }
+//     })
+// }
 
 
 }(jQuery));
